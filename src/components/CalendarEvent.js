2957f39a88bc568c9497a0a2a821f5dff82043f2
Jeff Hykin <jeff.hykin@gmail.com>
2019-10-05 06:35:00 -0500

    _

---
+++ src/components/CalendarEvent.js
@@ -0,0 +1,12 @@
+class CalendarEvent {
+    constructor({ date, time, repeatPattern }) {
+        this.date = new Date(date)
+        this.time = time
+        this.repeatPattern = repeatPattern
+    }
+    dateTime() {
+        return 
+    }
+}
+
+export default CalendarEvent
