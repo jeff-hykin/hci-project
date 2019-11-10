4682877ef78d98f4ea1e523f17c1ff90d411472e
Jeff Hykin <jeff.hykin@gmail.com>
2019-11-10 15:06:17 -0600

    -

--- src/App.vue
+++ src/App.vue
@@ -28,9 +28,14 @@ let globalDataDefaultValues = {
         {
             name: "Event1",
             description: "Do some stuff",
-            subTasks: [],
-            dateTime: new DateTime([2019, 12, 31, 23, 59, 59]),
-        }
+            subTasks: [
+                "do something beforehand",
+            ],
+            startDateTime: new DateTime([2019, 12, 31, 23, 59, 59]),
+            endDateTime: new DateTime([2020, 1, 1, 0, 0, 0]),
+            source: "School Calendar",
+            location: [ 30.618634 /*latitude*/ , -96.324496 /*longitude*/ ],
+        },
     ],
     weather: [
         
