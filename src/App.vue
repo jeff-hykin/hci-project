33d8a66c9825c66e3ed2aafb096b3bdb557108af
Jeff Hykin <jeff.hykin@gmail.com>
2019-11-10 10:03:30 -0600

    - add global data

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,38 @@
 <script>
 import Vue from 'vue'
+Vue.config.productionTip = false
 
 import KeenUI from 'keen-ui'
 import 'keen-ui/dist/keen-ui.css'
 Vue.use(KeenUI)
 
-import 'css-baseline/css/4.css'
+import 'css-baseline/css/3.css'
 import GoodVue from 'good-vue'
 Vue.use(GoodVue)
 
-Vue.config.productionTip = false
-
+// 
+// imported components
+// 
 import ToDo from './components/ToDo'
+let components = {
+    'to-do': ToDo,
+}
 
+// 
+// globally accessible data
+// 
+let globalDataDefaultValues = {
+    events: [],
+}
+Vue.mixin({
+    data: ()=>({
+        $global: globalDataDefaultValues
+    })
+})
+
+// 
+// helper functions
+// 
 let currentUnixTime = ()=>new Date().getTime()/1000
 let padZero = (amount)=> {
     if (amount < 10) {
@@ -21,105 +41,22 @@ let padZero = (amount)=> {
         return amount
     }
 }
+
+// 
+// App
+// 
 let App = {
     name: 'Main',
-    components: {
-        ToDo,
-    },
+    components,
     data: ()=> ({
-        timeUntil: null,
-        calendarEventName: null,
-        nextCalendarEventTime: null,
-        calendarEvent: {
-            time: null,
-            date: null,
-            title: null,
-        },
-        calendarEvents: [
-        ],
     }),
     computed: {
-        secondsUntil() {
-            return padZero(Math.floor(this.timeUntil % 60))
-        },
-        minutesUntil() {
-            return padZero(Math.floor((this.timeUntil/60) % 60))
-        },
-        hoursUntil() {
-            return Math.floor((this.timeUntil/3600) % 24)
-        },
-        daysUntil() {
-            return Math.floor((this.timeUntil/86400))
-        },
-        countdown() {
-            let time = this.secondsUntil
-            if (this.minutesUntil) {
-                time = `${this.minutesUntil}:${time}`
-            }
-            if (this.hoursUntil) {
-                time = `${this.hoursUntil}:${time}`
-            }
-            if (this.daysUntil) {
-                time = `${this.daysUntil}:${time}`
-            }
-            return time
-        }
     },
     watch: {
-        timeUntil() {
-            document.title = this.countdown
-        }
     },
     methods: {
-        timeUntilNextCaledarEvent() {
-            if (this.nextCalendarEventTime) {
-                return this.nextCalendarEventTime - currentUnixTime()
-            } else {
-                return -Infinity
-            }
-        },
-        updateCalendarEvents(tasks) {
-            // erase the calendar events
-            this.calendarEvents = []
-            for (let each of tasks) {
-                this.calendarEvents.push({ unixTime: each.dateTime.getTime()/1000, title: each.content })
-            }
-            // reset the time
-            this.nextCalendarEventTime = null
-            this.timeUntil = null
-            this.updateTime()
-        },
-        updateTime() {
-            let timeUntil = this.timeUntilNextCaledarEvent()
-            if (timeUntil > 0) {
-                this.timeUntil = timeUntil
-            } else {
-                // if no more events
-                if (!this.calendarEvents.length) {
-                    this.timeUntil = 0
-                // if there are calendarEvents 
-                } else if (this.calendarEvents.length) {
-                    // if not on the first event
-                    if (this.timeUntil != null) {
-                        // remove the event that was just completed
-                        this.calendarEvents.shift()
-                    }
-                    // schedule the next event
-                    if (this.calendarEvents[0]) {
-                        this.nextCalendarEventTime = this.calendarEvents[0].unixTime
-                        this.calendarEventName = this.calendarEvents[0].title
-                        // immediately update the time again (kinda recursion)
-                        this.updateTime()
-                    } else {
-                        this.timeUntil = 0
-                    }
-                }                
-            }
-        }
     },
     mounted() {
-        // start update loop
-        setInterval(this.updateTime,1000)
     },
 }
 // Since this is root, Load the app immediately after this file loads: setTimeout(0)
@@ -128,47 +65,9 @@ setTimeout(()=>(new (Vue.extend(App))).$mount('#app'),0)
 export default App
 </script>
 <template>
-    <row align-h=space-between align-v=top :wrap=true>
-        <!-- spacing -->
-        <!-- <column margin=2rem>
-        </column> -->
-        
-        <!-- timer area -->
-        <column margin=2rem>
-            <row class='card no-word-wrap' color=gray>
-                <h3 style='width: max-content'>{{calendarEventName}}</h3>
-            </row>
-            <row>
-                <h1 v-if="daysUntil">{{daysUntil}}:</h1>
-                <h1 v-if="hoursUntil">{{hoursUntil}}:</h1>
-                <h1 v-if="minutesUntil">{{minutesUntil}}:</h1>
-                <h1>{{secondsUntil}}</h1>
-            </row>
-        </column>
-        <!-- Events -->
-        <column class=event-container>
-            <!-- <column class=card>
-                <md-datepicker v-model="selectedDate" md-immediately />
-                <row class=card-row>
-                    <h5>Title</h5>
-                    <input type="text" v-model="calendarEvent.title">
-                </row>
-                <row class=card-row>
-                    <h5>Date</h5>
-                    <input type="date" v-model="calendarEvent.date">
-                </row>
-                <row class=card-row>
-                    <h5>Time</h5>
-                    <input type="time" v-model="calendarEvent.time">
-                </row>
-                <button @click="submitCalendarEvent">
-                    Submit
-                </button>
-            </column> -->
-            <row height=2rem />
-            <to-do @taskChange='updateCalendarEvents' />
-        </column>
-    </row>
+    <column align-v=top :wrap=true>
+        <h2>Test Title</h2>
+    </column>
 </template>
 <style scoped>
 .card {
