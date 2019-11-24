3971b23cf198b22983bbb7440a71db50695451b1
Jeff Hykin <jeff.hykin@gmail.com>
2019-11-24 17:01:39 -0600

    - notes styling complete

--- src/components/notes.vue
+++ src/components/notes.vue
@@ -4,18 +4,17 @@
         <container class='card notes shadow'/>
         <!-- actual notepad -->
         <container class='card notes' shadow=0>
-            <container class=card-head shadow=1>
+            <container class=card-head>
                 <h5>Notes</h5>
             </container>
             <container class=card-body>
-                <v-textarea rounded fill-height outlined v-model="global.notes" />
+                <textarea rows="6" cols="32" v-model="global.notes" />
             </container>
         </container>
     </container>
 </template>
 
 <script>
-let color = 'rgb(242,177,51)'
 export default {
     name: "Notes",
 }
@@ -28,9 +27,31 @@ export default {
     right: 0;
     bottom: -2.5rem;
     transform: rotate(var(--rotation));
-    background-color: rgb(242,177,51);
+    background-color: rgb(243, 195, 99);
     width: 20rem;
     height: 17rem;
+    border-radius: 0;
+    
+    .card-head {
+        padding-top: 0;
+        padding-bottom: 0;
+        box-shadow: none;
+        
+        h5 {
+            font-family: Shadows Into Light;
+            font-size: 30pt;
+            color: rgb(66, 77, 87);
+        }
+    }
+    
+    .card-body {
+        textarea {
+            resize: none;
+            font-family: Kalam;
+            line-height: 1.1em;
+            font-size: 14pt;
+        }
+    }
     
     &.shadow {
         transform: rotate(calc(var(--rotation) + 4deg + 90deg)) translate(35px, 9px) scaleX(0.95);
