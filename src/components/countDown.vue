8689733fd1c96e7f851f5e65f9fb6454beb91924
Jeff Hykin <jeff.hykin@gmail.com>
2019-11-25 09:35:14 -0600

    - improve countdown

--- src/components/countDown.vue
+++ src/components/countDown.vue
@@ -15,17 +15,21 @@ export default {
         countDown() {
             let eventSeconds = this.currentEvent.startDateTime.timeOfDayAsSeconds
             let currentSeconds = this.global.currentTime.timeOfDayAsSeconds
-            console.log(`this.global.currentTime is:`,this.global.currentTime)
-            console.log(`eventSeconds is:`,eventSeconds)
-            console.log(`currentSeconds is:`,currentSeconds)
-            console.log(`this.global.currentTime.unix - currentSeconds*1000 is:`,this.global.currentTime.unix - currentSeconds*1000)
             let miliseconds =  (this.global.currentTime.unix - currentSeconds*1000) + eventSeconds*1000 - this.global.currentTime.unix
             let negative = miliseconds < 0
             miliseconds = Math.abs(miliseconds)
             let hours   = Math.trunc(miliseconds/(1000*60*60))
             let minutes = Math.trunc(miliseconds/(1000*60) - hours*60)
             let seconds = Math.trunc(miliseconds/(1000)    - hours*60*60 - minutes*60)
-            return `${negative?'-':''}${hours}:${padZero(minutes)}:${padZero(seconds)}`
+            let output
+            if (hours == 0) {
+                output = `${minutes}:${padZero(seconds)}`
+            } else {
+                output = `${negative?'-':''}${hours}:${padZero(minutes)}:${padZero(seconds)}`
+            }
+            // set the title of the page
+            setTimeout(() => document.title = output , 0)
+            return output
         }
     }
 }
